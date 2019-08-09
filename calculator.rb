deb54993528fa5d7f789094eef7bf78486328f52
asuka001 <a.asuka54@gmail.com>
2019-08-09 11:00:04 +0000

    first commit

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,16 @@ puts "input waiting..."
 input = gets # 標準入力を受け付ける
 result = input.chomp == 'answer' # inputには改行文字が含まれているためchompしています
 puts result
+
+
+setting = gets.to_i 
+opentime = gets.to_i
+shutdowntime = gets.to_i
+
+def pre_value
+  if opentime < setting
+    sum = opentime * 0.8
+  else
+    sum = (opentime - setting) * 0.2 + setting * 0.8
+  end
+end
